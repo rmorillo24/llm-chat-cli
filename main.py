3b0f1281e6b000e9ae75178484bd131550840db1
rmorillo24 <rmorillo@gmail.com>
2025-09-09 08:30:20 +0200

    update ollama config, and make ConfigManager

--- main.py
+++ main.py
@@ -3,27 +3,24 @@
 import os
 import yaml
 import timeit
+import subprocess
 from pathlib import Path
 from clients import BaseChatClient, XaiChat, GeminiChat, OpenAiCompatibleChat, OpenAiChat
+from utils import ConfigManager
 from typing import Dict, Any, List
 from rich.console import Console
 from rich.markdown import Markdown
 import logging
 import questionary
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 # logging.debug("Variable x = %s", x)
 
 class LLMClient:
-    def __init__(self, config_path: str):
-        try:
-            with open(config_path, 'r') as file:
-                self.config = yaml.safe_load(file)
-                logging.debug("Loaded config: %s", self.config)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Config file not found: {config_path}")
-        except yaml.YAMLError as e:
-            raise ValueError(f"Error parsing the ysaml file: {e}")
+    def __init__(self, config_manager: ConfigManager):
+        self.config_manager = config_manager 
+        self.config = config_manager.get_config()
+
         self.clients = {client['type']: client for client in self.config.get('clients', [])}
         self.default_model = self.config.get('default', None)
         self.current_client: Optional[BaseChatClient] = None
@@ -36,10 +33,12 @@ class LLMClient:
         model = model or self.default_model
         logging.debug("load_model -> loading model: %s", model)
         if not model:
-            raise ValueError("Model not specified abd degault model not present")
+            raise ValueError("Model not specified and default model not present")
 
-        provider, model_name = model.split(':') if ':' in model else (None, model)
+        provider, model_name, *version = model.split(':') if ':' in model else (None, model)
+        if version: model_name += ":" + version[0]
         logging.debug("load_model -> provider: %s, model_name: %s", provider, model_name)
+
         if not provider or not model_name:
             raise ValueError(f"Invalid format: {model}. Expecting 'provider:model'")
         if provider not in self.clients:
@@ -81,16 +80,22 @@ class LLMClient:
                 models.append(provider["type"] + ":" + model['name'] )
         return models
 
+    def get_config(self):
+        return self.config
 
+
+           
 class CommandHandler:
-    def __init__(self):
+    def __init__(self, config_manager: ConfigManager):
         # Initialize the dispatch table with reserved words and their actions
         self.commands = {
             '.exit': self._exit,
             '.help': self._help,
             '.clear': self._clear,
-            '.models': self._models
+            '.models': self._models,
+            '.updateollama': self._update_ollama_models
         }
+        self.config_manager = config_manager
 
     def _exit(self):
         print("Bye!")
@@ -111,6 +116,10 @@ class CommandHandler:
             choices = models
         ).ask()
         llm_client.load_model(selected_model)
+        return True
+
+    def _update_ollama_models(self):
+        self.config_manager.update_ollama_models()
         return True
 
     def handle_input(self, user_input):
@@ -141,13 +150,14 @@ class CommandHandler:
 
 
 if __name__ == "__main__":
-    config_path = Path.home() / Path(".config/llm-chat-cli/configs.yaml")
+
+    config_manager= ConfigManager(Path.home() / ".config" / "llm-chat-cli" / "configs.yaml")
     console = Console()
 
     try:
-        llm_client = LLMClient(config_path)
+        llm_client = LLMClient(config_manager)
+        llm_client.load_model()  # Load default model for faster start
         console.print(Markdown(f"Chatting with *" + llm_client.default_model + "*. How can I help you today?"))
-        llm_client.load_model()  # Carga el modelo por defecto
     except Exception as e:
         print(f"Error al cargar la configuración: {e}")
         exit(1)
@@ -156,10 +166,13 @@ if __name__ == "__main__":
         {'role': 'system', 'content': 'You are my assistant. I want short and concise answers without decoration or polite, unnecessary words.'}
     ]
 
-    command_handler = CommandHandler()
+    command_handler = CommandHandler(config_manager)
     st = True
     while st:
         user_input = input("\n> ")
-        st = command_handler.handle_input(user_input)
+        try:
+            st = command_handler.handle_input(user_input)
+        except Exception as e:
+            print(f"ERROR: {e}")
         
 
